ac99e2ac1417a98d30f6a4f5fb55999bf5d169d1
DezFix <97520208+DezFix@users.noreply.github.com>
2025-07-25 12:02:08 +0300

    Add files via upload

---
+++ apps.ps1
@@ -0,0 +1,61 @@
+{
+  "Presets": {
+    "Standard": [
+      {"Id": "sumatrapdf", "Name": "SumatraPDF"},
+      {"Id": "7zip", "Name": "7-Zip"},
+      {"Id": "anydesk", "Name": "AnyDesk"},
+      {"Id": "notepadplusplus", "Name": "Notepad++"},
+      {"Id": "vlc", "Name": "VLC Media Player"}
+    ],
+    "Gamer": [
+      {"Id": "steam", "Name": "Steam"},
+      {"Id": "discord", "Name": "Discord"},
+      {"Id": "obs-studio", "Name": "OBS Studio"}
+    ],
+    "Optional": [
+      {"Id": "onlyoffice", "Name": "Onlyoffice"},
+      {"Id": "gimp", "Name": "GIMP"},
+      {"Id": "inkscape", "Name": "Inkscape"},
+      {"Id": "audacity", "Name": "Audacity"}
+    ]
+  },
+  "ManualCategories": {
+    "Браузеры": [
+      {"Name": "Opera", "Id": "opera"},
+      {"Name": "Google Chrome", "Id": "googlechrome"},
+      {"Name": "Mozilla Firefox", "Id": "firefox"},
+      {"Name": "Tor Browser", "Id": "tor-browser"}
+    ],
+    "Инструменты": [
+      {"Name": "SumatraPDF", "Id": "sumatrapdf"},
+      {"Name": "7-Zip", "Id": "7zip"},
+      {"Name": "AnyDesk", "Id": "anydesk"},
+      {"Name": "Notepad++", "Id": "notepadplusplus"},
+      {"Name": "VLC Media Player", "Id": "vlc"},
+      {"Name": "GIMP", "Id": "gimp"},
+      {"Name": "Inkscape", "Id": "inkscape"},
+      {"Name": "Audacity", "Id": "audacity"}
+    ],
+    "Связь": [
+      {"Name": "Telegram", "Id": "telegram"},
+      {"Name": "Viber", "Id": "viber"},
+      {"Name": "Microsoft Teams", "Id": "msteams"},
+      {"Name": "Zoom", "Id": "zoom"},
+      {"Name": "Signal", "Id": "signal"}
+    ],
+    "Игры и Связь": [
+      {"Name": "Steam", "Id": "steam"},
+      {"Name": "Discord", "Id": "discord"},
+      {"Name": "OBS Studio", "Id": "obs-studio"}
+    ],
+    "Офис": [
+      {"Name": "Onlyoffice", "Id": "onlyoffice"},
+      {"Name": "LibreOffice", "Id": "libreoffice"}
+    ],
+    "Разработка": [
+      {"Name": "VS Code", "Id": "vscode"},
+      {"Name": "Git", "Id": "git"},
+      {"Name": "Python", "Id": "python"}
+    ]
+  }
+}
