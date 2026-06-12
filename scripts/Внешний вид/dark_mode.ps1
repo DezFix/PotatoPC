a78ae030a5d0aa3d63f805903415e09e2a3fe29e
DezFix <97520208+DezFix@users.noreply.github.com>
2026-06-12 10:39:56 +0300

    Update dark_mode.ps1

--- scripts/Внешний вид/dark_mode.ps1
+++ scripts/Внешний вид/dark_mode.ps1
@@ -1,6 +1,6 @@
 ﻿# NAME: Включить тёмную тему
 # DESC: Активирует тёмную тему для приложений и системы
-# CATEGORY: Внешний вид
+# TAGS: 1
 # ICON: 🌙
 
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0 -Type DWord -Force
