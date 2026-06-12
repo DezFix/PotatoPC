0704b7550c0d58f1d67f931a3812ae727c6b2250
DezFix <97520208+DezFix@users.noreply.github.com>
2026-06-12 07:18:22 +0300

    Delete scripts directory

--- scripts/dark_mode.ps1
+++
@@ -1,8 +0,0 @@
-﻿# NAME: Включить тёмную тему
-# DESC: Активирует тёмную тему для приложений и системы
-# CATEGORY: Внешний вид
-# ICON: 🌙
-
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0 -Type DWord -Force
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0 -Type DWord -Force
-Write-Host "Тёмная тема включена"
