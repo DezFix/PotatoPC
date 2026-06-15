6eb920f45340c9dbe7146b0c43d08037b8f88c32
DezFix <97520208+DezFix@users.noreply.github.com>
2026-06-15 07:18:33 +0300

    Update classic_context_menu.ps1

--- scripts/Внешний вид/classic_context_menu.ps1
+++ scripts/Внешний вид/classic_context_menu.ps1
@@ -1,7 +1,8 @@
 ﻿# NAME: Классическое контекстное меню (Win 11)
 # DESC: Возвращает старое меню правой кнопки мыши в Windows 11
-# TAGS: 1
+# TAGS: 1 , win11
 # ICON: 🖱️
+
 
 $path = "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32"
 if (!(Test-Path $path)) { New-Item -Path $path -Force | Out-Null }
